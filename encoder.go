8979d71a8d9e0f1dbdcc2139a6eb575767a9b755
Gentry Rolofson <xdarkicex@gmail.com>
2017-09-19 23:02:04 -0700

    adding uint

--- encoder.go
+++ encoder.go
@@ -48,3 +48,25 @@ func Base64ToUInt64(base64 string) uint64 {
 	}
 	return number
 }
+
+func UIntToBase64(number uint) string {
+	str := ""
+	for number > 0 {
+		r := number % uint(base)
+		number -= r
+		number /= uint(base)
+		str = string(b64[r]) + str
+	}
+	return str
+}
+
+func Base64ToUInt(base64 string) uint {
+	number := uint(0)
+	sliced := strings.Split(base64, "")
+	for _, v := range sliced {
+		r := strings.Index(b64, v)
+		number *= uint(base)
+		number += uint(r)
+	}
+	return number
+}
